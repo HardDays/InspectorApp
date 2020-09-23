aaecce2e520a7d654edb6f1cd0c8ff999d216a7f
Fekz115 <fekz115@gmail.com>
2020-09-23 17:01:20 +0300

    flutter: add project text styles

---
+++ lib/style/text_style.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/rendering.dart';
+import 'package:inspector/style/colors.dart';
+
+class ProjectTextStyles {
+  
+  static const title = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Montserrat",
+    fontSize: 18,
+    height: 24.0,
+  );
+  
+  static const subTitle = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Montserrat",
+    fontSize: 16,
+    height: 22.0,
+  );
+  
+  static const base = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Montserrat",
+    fontSize: 14,
+    height: 18.0,
+  );
+
+  static const baseBold = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Montserrat",
+    fontSize: 14,
+    height: 18.0,
+  );
+
+  static const medium = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Montserrat",
+    fontSize: 13,
+    height: 18.0,
+  );
+
+  static const mediumBold = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Montserrat",
+    fontSize: 18,
+    height: 24.0,
+  );
+
+  static const small = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Montserrat",
+    fontSize: 12,
+    height: 14.0,
+  );
+
+  static const smallBold = TextStyle(
+    color: ProjectColors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Montserrat",
+    fontSize: 12,
+    height: 16.0,
+  );
+
+}
