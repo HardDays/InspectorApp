8ff27681073372b4bd103e5704b6d46e25156dff
Fekz115 <fekz115@gmail.com>
2020-09-23 17:01:05 +0300

    flutter: add project colors

---
+++ lib/style/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class ProjectColors {
+  static const blue = Color(0x4375A0);
+  static const darkBlue = Color(0x7391AB);
+  static const mediumBlue = Color(0xB9C4D0);
+  static const lightBlue = Color(0xD2DDEA);
+  static const white = Color(0xFFFFFF);
+  static const black = Color(0x45474B);
+  static const grey = Color(0xF6F8F9);
+  static const red = Color(0xE50909);
+  static const green = Color(0x58AA43);
+  static const yellow = Color(0xFDA91A);
+  static const cyan = Color(0x07A6BA);
+}
