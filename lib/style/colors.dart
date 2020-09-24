55ac0256080c44a461c483a57d4d03d09362551f
Fekz115 <fekz115@gmail.com>
2020-09-24 06:26:07 +0300

    flutter: make colors with no opacity

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -1,15 +1,15 @@
 import 'dart:ui';
 
 class ProjectColors {
-  static const blue = Color(0x4375A0);
-  static const darkBlue = Color(0x7391AB);
-  static const mediumBlue = Color(0xB9C4D0);
-  static const lightBlue = Color(0xD2DDEA);
-  static const white = Color(0xFFFFFF);
-  static const black = Color(0x45474B);
-  static const grey = Color(0xF6F8F9);
-  static const red = Color(0xE50909);
-  static const green = Color(0x58AA43);
-  static const yellow = Color(0xFDA91A);
-  static const cyan = Color(0x07A6BA);
+  static const blue = Color(0xFF4375A0);
+  static const darkBlue = Color(0xFF7391AB);
+  static const mediumBlue = Color(0xFFB9C4D0);
+  static const lightBlue = Color(0xFFD2DDEA);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF45474B);
+  static const grey = Color(0xFFF6F8F9);
+  static const red = Color(0xFFE50909);
+  static const green = Color(0xFF58AA43);
+  static const yellow = Color(0xFFFDA91A);
+  static const cyan = Color(0xFF07A6BA);
 }
