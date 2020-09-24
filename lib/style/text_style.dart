c650894e6441932c7c37ea9d9423e38f63b60e62
Fekz115 <fekz115@gmail.com>
2020-09-24 06:25:47 +0300

    flutter: import fonts

--- lib/style/text_style.dart
+++ lib/style/text_style.dart
@@ -1,70 +1,68 @@
 import 'package:flutter/rendering.dart';
-import 'package:inspector/style/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProjectTextStyles {
-  
-  static const title = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w600,
-    fontFamily: "Montserrat",
-    fontSize: 18,
-    height: 24.0,
-  );
-  
-  static const subTitle = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w600,
-    fontFamily: "Montserrat",
-    fontSize: 16,
-    height: 22.0,
-  );
-  
-  static const base = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w400,
-    fontFamily: "Montserrat",
-    fontSize: 14,
-    height: 18.0,
+  static final title = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      height: 24.0/18.0,
+    ),
   );
 
-  static const baseBold = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w600,
-    fontFamily: "Montserrat",
-    fontSize: 14,
-    height: 18.0,
+  static final subTitle = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      height: 22.0/16.0,
+    ),
   );
 
-  static const medium = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w400,
-    fontFamily: "Montserrat",
-    fontSize: 13,
-    height: 18.0,
+  static final base = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 14,
+      height: 18.0/14.0,
+    ),
   );
 
-  static const mediumBold = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w600,
-    fontFamily: "Montserrat",
-    fontSize: 18,
-    height: 24.0,
+  static final baseBold = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+      height: 18.0/14.0,
+    ),
   );
 
-  static const small = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w400,
-    fontFamily: "Montserrat",
-    fontSize: 12,
-    height: 14.0,
+  static final medium = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 13,
+      height: 18.0/13.0,
+    ),
   );
 
-  static const smallBold = TextStyle(
-    color: ProjectColors.black,
-    fontWeight: FontWeight.w600,
-    fontFamily: "Montserrat",
-    fontSize: 12,
-    height: 16.0,
+  static final mediumBold = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      height: 24.0/18.0,
+    ),
   );
 
+  static final small = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 12,
+      height: 14.0/12.0,
+    ),
+  );
+
+  static final smallBold = GoogleFonts.montserrat(
+    textStyle: TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 12,
+      height: 16.0/12.0,
+    ),
+  );
 }
