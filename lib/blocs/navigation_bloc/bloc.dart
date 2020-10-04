64d0fe306593d9db706b0de3a6136d572c19783e
Fekz115 <fekz115@gmail.com>
2020-10-04 11:41:22 +0300

    flutter: open YandexMap from bottom navigation bar

--- lib/blocs/navigation_bloc/bloc.dart
+++ lib/blocs/navigation_bloc/bloc.dart
@@ -1,15 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:inspector/blocs/navigation_bloc/events.dart';
+import 'package:inspector/blocs/navigation_bloc/screens.dart';
 import 'package:inspector/blocs/navigation_bloc/states.dart';
 
 class NavigationBloc extends Bloc<NavigationBlocEvent, NavigationBlocState> {
-
   NavigationBloc(initialState) : super(initialState);
 
+  static const platform = const MethodChannel('com.example.inspector/map');
+
   @override
-  Stream<NavigationBlocState> mapEventToState(NavigationBlocEvent event) async* {
-    if(event is ChangeScreen)
-      yield(BottomNavigationStateChanged(event.screen));
+  Stream<NavigationBlocState> mapEventToState(
+      NavigationBlocEvent event) async* {
+    if (event is ChangeScreen) {
+      if (event.screen == Screens.MapScreen)
+        _openMap();
+      else
+        yield (BottomNavigationStateChanged(event.screen));
+    }
   }
 
+  Future<void> _openMap() async {
+    await platform.invokeMethod('openYandexMap');
+  }
 }
