304de6b07f927b39c607be62baa3ee9451321a55
hz <shvv733kn@gmail.com>
2020-10-05 11:05:12 +0300

    before merge

--- lib/pages/control_list_page.dart
+++ lib/pages/control_list_page.dart
@@ -63,66 +63,69 @@ class ControlListPageState extends State<ControlListPage> {
           ),
           Expanded(
             child: mapContent ? 
-            Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                FlutterMap(
-                  options: MapOptions(
-                    center: LatLng(55.746875, 37.6200),
-                    zoom: 16.5  ,
-                  ),
-                  layers: [
-                    TileLayerOptions(
-                      urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      subdomains: ['a', 'b', 'c']
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(55.746875, 37.6200),
+                      zoom: 16.5  ,
                     ),
-                    PolygonLayerOptions(
-                      polygons: [
-                        Polygon(
-                          color: ProjectColors.red.withOpacity(0.5),
-                          borderColor: ProjectColors.blue,
-                          borderStrokeWidth: 1,
-                          points: [
-                            LatLng(55.7479487, 37.6207602),
-                            LatLng(55.7481479, 37.6206958),
-                            LatLng(55.7479849, 37.6194942),
-                            LatLng(55.7459921, 37.6205993),
-                            LatLng(55.7461612, 37.6208138),
-                            LatLng(55.7477554, 37.6199126),
-                            LatLng(55.7477192, 37.6202667),
-                            LatLng(55.7478581, 37.620374),
-                            LatLng(55.7479849, 37.620728),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      color: Colors.white,
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          _buildInfo(),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10, right: 20, left: 20, bottom: 10),
-                            child: Row(
-                              children: [
-                                ProjectButton.buildOutlineButton('Нарушений не выявлено', color: ProjectColors.green, onPressed: ()=>{}),
-                                Padding(padding: const EdgeInsets.only(left: 20)),
-                                ProjectButton.buildOutlineButton('Зафиксировать нарушение', color: ProjectColors.red, onPressed: ()=>{})
-                              ],
-                            ),
+                    layers: [
+                      TileLayerOptions(
+                        urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        subdomains: ['a', 'b', 'c']
+                      ),
+                      PolygonLayerOptions(
+                        polygons: [
+                          Polygon(
+                            color: ProjectColors.red.withOpacity(0.5),
+                            borderColor: ProjectColors.blue,
+                            borderStrokeWidth: 1,
+                            points: [
+                              LatLng(55.7479487, 37.6207602),
+                              LatLng(55.7481479, 37.6206958),
+                              LatLng(55.7479849, 37.6194942),
+                              LatLng(55.7459921, 37.6205993),
+                              LatLng(55.7461612, 37.6208138),
+                              LatLng(55.7477554, 37.6199126),
+                              LatLng(55.7477192, 37.6202667),
+                              LatLng(55.7478581, 37.620374),
+                              LatLng(55.7479849, 37.620728),
+                            ],
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        color: Colors.white,
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            _buildInfo(),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10, right: 20, left: 20, bottom: 10),
+                              child: Row(
+                                children: [
+                                  ProjectButton.buildOutlineButton('Нарушений не выявлено', color: ProjectColors.green, onPressed: ()=>{}),
+                                  Padding(padding: const EdgeInsets.only(left: 20)),
+                                  ProjectButton.buildOutlineButton('Зафиксировать нарушение', color: ProjectColors.red, onPressed: ()=>{})
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ) :
             ListView(
               padding: const EdgeInsets.only(top: 20),
