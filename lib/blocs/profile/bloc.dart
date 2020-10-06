b99ffcd78c5a58e0c16a7cfbbb3790d3308024ab
Fekz115 <fekz115@gmail.com>
2020-10-06 17:18:33 +0300

    flutter: bloc: bloc for profile page

---
+++ lib/blocs/profile/bloc.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inspector/blocs/profile/event.dart';
+import 'package:inspector/blocs/profile/state.dart';
+import 'package:inspector/model/user.dart';
+import 'package:inspector/services/persistance_service.dart';
+
+class ProfileBloc extends Bloc<ProfileBlocEvent, ProfileBlocState> {
+  ProfileBloc(ProfileBlocState initialState, this._persistanceService)
+      : super(initialState);
+
+  final PersistanceService _persistanceService;
+
+  @override
+  Stream<ProfileBlocState> mapEventToState(ProfileBlocEvent event) async* {
+    if (event is SetUsingFingerPrint) {
+      await _persistanceService.saveFingerprintState(event.useFingerPrint);
+      FilledBlocState prev = state as FilledBlocState;
+      yield FilledBlocState(
+        appVersion: prev.appVersion,
+        dataSendingMode: prev.dataSendingMode,
+        dataSendingState: prev.dataSendingState,
+        installDate: prev.installDate,
+        lastDataSendingDate: prev.lastDataSendingDate,
+        useFingerprint: event.useFingerPrint,
+        userName: prev.userName,
+      );
+    } else if (event is SetDataSendingMode) {
+      FilledBlocState prev = state as FilledBlocState;
+      await _persistanceService.saveDataSendingState(event.dataSendingMode);
+      yield FilledBlocState(
+        appVersion: prev.appVersion,
+        dataSendingMode: event.dataSendingMode,
+        dataSendingState: prev.dataSendingState,
+        installDate: prev.installDate,
+        lastDataSendingDate: prev.lastDataSendingDate,
+        useFingerprint: prev.useFingerprint,
+        userName: prev.userName,
+      );
+    }
+    if (event is InitEvent) {
+      User user = await _persistanceService.getUser();
+      String name =
+          '${user.position} ${user.surname} ${user.name} ${user.middleName}';
+      DateTime lastDataSendingDate =
+          await _persistanceService.getLastDataSendingDate();
+      bool useFingerPrint = await _persistanceService.getFingerprintState();
+      bool dataSendingMode = await _persistanceService.getDataSendingState();
+      yield FilledBlocState(
+        appVersion: '001-0124',
+        dataSendingMode: dataSendingMode == null ? false : dataSendingMode,
+        dataSendingState: 'Ошибка',
+        installDate: await _getInstallDate(),
+        lastDataSendingDate: lastDataSendingDate,
+        useFingerprint: useFingerPrint == null ? true : useFingerPrint,
+        userName: name,
+      );
+    }
+  }
+
+  Future<DateTime> _getInstallDate() async {
+    return DateTime(2020, 7, 15, 21, 55);
+  }
+}
---
+++ lib/blocs/profile/event.dart
@@ -0,0 +1,15 @@
+abstract class ProfileBlocEvent {}
+
+class SetUsingFingerPrint extends ProfileBlocEvent {
+  final bool useFingerPrint;
+
+  SetUsingFingerPrint(this.useFingerPrint);
+}
+
+class SetDataSendingMode extends ProfileBlocEvent {
+  final bool dataSendingMode;
+
+  SetDataSendingMode(this.dataSendingMode);
+}
+
+class InitEvent extends ProfileBlocEvent {}
