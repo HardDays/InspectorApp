8fba5c5a4a8a470cd765018ef71cd25fd8d5fa98
Fekz115 <fekz115@gmail.com>
2021-03-01 17:23:54 +0300

    flutter: style: section: display description and child both

--- lib/style/section.dart
+++ lib/style/section.dart
@@ -3,8 +3,7 @@ import 'package:inspector/style/colors.dart';
 import 'package:inspector/style/divider.dart';
 import 'package:inspector/style/text_style.dart';
 
-class ProjectSection extends StatelessWidget { 
-
+class ProjectSection extends StatelessWidget {
   final String title;
   final String description;
   final Widget child;
@@ -20,18 +19,29 @@ class ProjectSection extends StatelessWidget {
             Container(
               width: MediaQuery.of(context).size.width * 0.3,
               margin: const EdgeInsets.all(16),
-              child: Text(title,
-                style: ProjectTextStyles.baseBold.apply(color: ProjectColors.blue),
+              child: Text(
+                title,
+                style:
+                    ProjectTextStyles.baseBold.apply(color: ProjectColors.blue),
               ),
             ),
             Expanded(
               flex: 1,
               child: Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 16, right: 16),
-                child: description != null ? Text(description,  
-                  style: ProjectTextStyles.base.apply(color: ProjectColors.black),
-                ) : child,
-              )
+                child: Row(
+                  children: [
+                    if(description != null)
+                      Text(
+                        description,
+                        style: ProjectTextStyles.base
+                            .apply(color: ProjectColors.black),
+                      ),
+                    if(child != null)
+                      child,
+                  ],
+                ),
+              ),
             ),
           ],
         ),
