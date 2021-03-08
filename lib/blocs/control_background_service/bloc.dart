66904b9484a011055dbe925714342eba712c9c3c
Fekz115 <fekz115@gmail.com>
2021-03-08 18:27:15 +0300

    feat: check network status before loading objects

--- lib/blocs/control_background_service/bloc.dart
+++ lib/blocs/control_background_service/bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:async/async.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:inspector/blocs/background_loading/bloc.dart';
@@ -7,6 +9,8 @@ import 'package:inspector/blocs/control_background_service/state.dart';
 import 'package:inspector/blocs/notification_bloc/bloc.dart';
 import 'package:inspector/blocs/notification_bloc/events.dart';
 import 'package:inspector/services/department_control/department_control_service.dart';
+import 'package:inspector/services/network_status_service/network_status.dart';
+import 'package:inspector/services/network_status_service/network_status_service.dart';
 
 class ControlBackgroundServiceBloc extends Bloc<
     ControlBackgroundServiceBlocEvent, ControlBackgroundServiceBlocState> {
@@ -14,13 +18,22 @@ class ControlBackgroundServiceBloc extends Bloc<
     this.departmentControlService,
     this.notificationBloc,
     this.backgroundLoadingBloc,
+    this.networkStatusService,
   }) : super(ControlBackgroundServiceBlocState.emptyState()) {
     add(ControlBackgroundServiceBlocEvent.initEvent());
+    _networkStatusStreamSubscription =
+        networkStatusService.listenNetworkStatus.listen((value) {
+      _networkStatus = value;
+    });
   }
 
   final DepartmentControlService departmentControlService;
   final NotificationBloc notificationBloc;
   final BackgroundLoadingBloc backgroundLoadingBloc;
+  final NetworkStatusService networkStatusService;
+
+  StreamSubscription<NetworkStatus> _networkStatusStreamSubscription;
+  NetworkStatus _networkStatus;
 
   @override
   Stream<ControlBackgroundServiceBlocState> mapEventToState(
@@ -33,11 +46,14 @@ class ControlBackgroundServiceBloc extends Bloc<
 
   Stream<ControlBackgroundServiceBlocState> _onInitEvent(
       InitEvent event) async* {
-    final metadata = await departmentControlService.localMetadata;
-    if (metadata.loaded) {
-      yield ControlBackgroundServiceBlocState.loaded(metadata.lastUpdatedDate);
-    } else {
-      yield ControlBackgroundServiceBlocState.notLoaded();
+    if (_networkStatus?.connectionStatus == ConnectionStatus.online) {
+      final metadata = await departmentControlService.localMetadata;
+      if (metadata.loaded) {
+        yield ControlBackgroundServiceBlocState.loaded(
+            metadata.lastUpdatedDate);
+      } else {
+        yield ControlBackgroundServiceBlocState.notLoaded();
+      }
     }
   }
 
@@ -66,7 +82,8 @@ class ControlBackgroundServiceBloc extends Bloc<
               ),
             );
           });
-          notificationBloc.add(SnackBarNotificationEvent('Объекты ведомстенного контроля загружены'));
+          notificationBloc.add(SnackBarNotificationEvent(
+              'Объекты ведомстенного контроля загружены'));
         }),
         onCancel: departmentControlService.cancelLoading,
       );
