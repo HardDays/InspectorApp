f7aaf484eec47c4a90646361e3ed646465d356b6
Fekz115 <fekz115@gmail.com>
2021-04-05 13:56:48 +0300

    fix(dc widgets): show date in new line [8139-15]

--- lib/widgets/control/control_object/search_result/search_result_widget.dart
+++ lib/widgets/control/control_object/search_result/search_result_widget.dart
@@ -47,7 +47,7 @@ class SearchResultWidget extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Дата обследования ${DateFormat("hh:mm dd.MM.yyyy").format(searchResult.surveyDate)}',
+                          'Дата обследования\n${DateFormat("hh:mm dd.MM.yyyy").format(searchResult.surveyDate)}',
                           style: ProjectTextStyles.baseBold
                               .apply(color: ProjectColors.blue),
                         ),
