a864cedb21edb7f720e1618dfafc03a8728d42c0
Fekz115 <fekz115@gmail.com>
2021-04-18 16:47:14 +0300

    Revert "fix(dc object page): correct sections [8140-27]"
    This reverts commit 56c74f436746fa3a6eac39782642e88d7caba037.

--- lib/widgets/control/control_object/control_object_info.dart
+++ lib/widgets/control/control_object/control_object_info.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:inspector/model/department_control/control_object.dart';
 import 'package:inspector/style/colors.dart';
 import 'package:inspector/style/section.dart';
-import 'package:inspector/style/text_style.dart';
 import 'package:inspector/widgets/control/control_object/status.dart';
 
 class ControlObjectInfo extends StatelessWidget {
@@ -18,31 +17,16 @@ class ControlObjectInfo extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16, right: 16),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(width: 16),
-                ControlStatusWidget(
-                  controlObject.type.name,
-                  controlObject.id.toString(),
-                ),
-                SizedBox(width: 16),
-                Text(
-                  controlObject.address,
-                  style: ProjectTextStyles.base.apply(color: ProjectColors.black),
-                ),
-              ],
-            ),
-          ),
-        ),
-        if (controlObject.kind != null && controlObject.kind.isNotEmpty)
+        ProjectSection('Объект',
+            child: ControlStatusWidget(controlObject.type.name,
+                controlObject.id.toString())),
+        _buildDivider(),
+        ProjectSection('Адрес', description: controlObject.address),
+        if(controlObject.kind != null && controlObject.kind.isNotEmpty)
           _buildDivider(),
-        if (controlObject.kind != null && controlObject.kind.isNotEmpty)
-          ProjectSection('Вид объекта', description: controlObject.kind),
+        if(controlObject.kind != null && controlObject.kind.isNotEmpty)
+          ProjectSection('Вид объекта',
+              description: controlObject.kind),
         _buildDivider(),
         ProjectSection('Балансодержатель',
             description: controlObject.balanceOwner),
