f2898ae7e5fc8d2ce0d6a0f3ff8fc39b2b1664a4
Fekz115 <fekz115@gmail.com>
2021-04-23 00:23:41 +0300

    fix(dc): move reason field to top in violation resolve date extension form [8140-22]

--- lib/widgets/control/control_object/violation/extension_period_form.dart
+++ lib/widgets/control/control_object/violation/extension_period_form.dart
@@ -7,7 +7,6 @@ import 'package:inspector/style/button.dart';
 import 'package:inspector/style/date_picker.dart';
 import 'package:inspector/style/text_field.dart';
 import 'package:inspector/style/text_style.dart';
-import 'package:provider/provider.dart';
 
 class ExtensionPeriodForm extends StatefulWidget {
   const ExtensionPeriodForm({
@@ -33,82 +32,94 @@ class _ExtensionPeriodFormState extends State<ExtensionPeriodForm> {
   TextEditingController _commentsController = TextEditingController();
   TextEditingController _reasonController = TextEditingController();
   DateTime _newResolveDate = DateTime.now();
+  GlobalKey<FormState> _formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    if(_reasonController.text != _extensionReason.name) {
+    if (_reasonController.text != _extensionReason.name) {
       _reasonController.text = _extensionReason.name;
     }
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          'Продление срока устранения нарушения',
-          style: ProjectTextStyles.title,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ProjectTextField(
-          controller: _commentsController,
-          enabled: true,
-          hintText: 'Комментарий',
-          maxLines: 4,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ProjectAutocomplete<ViolationExtensionReason>(
-          'Основание продления срока устранения нарушения',
-          controller: _reasonController,
-          enabled: true,
-          formatter: (a) => a.name,
-          onSuggestionSelected: (reason) => setState(() {
-            _extensionReason = reason;
-          }),
-          suggestionsCallback: (value) async {
-            final t = await widget.dictionaryService
-                .getViolationExtensionReasons(name: value);
-            return t;
-          },
-          onChanged: (value) => setState(() {
-            _extensionReason = ViolationExtensionReason(name: value);
-          }),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ProjectDatePicker(
-          title: 'Новый срок устранения',
-          hintText: 'Выберите дату',
-          values: [_newResolveDate],
-          onChanged: (value) => setState(() {
-            _newResolveDate = value.first;
-          }),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ProjectButton.buildOutlineButton('Отменить',
-                onPressed: widget.onCancel),
-            SizedBox(width: 20),
-            ProjectButton.builtFlatButton(
-              'ОК',
-              onPressed: () => widget.onConfirm(
-                ViolationExtensionPeriod(
-                  comments: _commentsController.value.text,
-                  extensionReason: _extensionReason,
-                  resolveDate: _newResolveDate,
-                ),
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Продление срока устранения нарушения',
+            style: ProjectTextStyles.title,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ProjectAutocomplete<ViolationExtensionReason>(
+            'Основание продления срока устранения нарушения',
+            controller: _reasonController,
+            enabled: true,
+            formatter: (a) => a.name,
+            onSuggestionSelected: (reason) => setState(() {
+              _extensionReason = reason ?? ViolationExtensionReason(name: '');
+            }),
+            suggestionsCallback: (value) async {
+              final t = await widget.dictionaryService
+                  .getViolationExtensionReasons(name: value);
+              return t;
+            },
+            onChanged: (value) => setState(() {
+              _extensionReason = ViolationExtensionReason(name: value);
+            }),
+            validator: (_) => _extensionReason.id == null
+                ? 'Основание для продления срока нарушения\nдолжно быть выбрано из выпадающего списка'
+                : null,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ProjectTextField(
+            controller: _commentsController,
+            enabled: true,
+            hintText: 'Комментарий',
+            maxLines: 4,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ProjectDatePicker(
+            title: 'Новый срок устранения',
+            hintText: 'Выберите дату',
+            values: [_newResolveDate],
+            onChanged: (value) => setState(() {
+              _newResolveDate = value.first;
+            }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ProjectButton.buildOutlineButton('Отменить',
+                  onPressed: widget.onCancel),
+              SizedBox(width: 20),
+              ProjectButton.builtFlatButton(
+                'ОК',
+                onPressed: () {
+                  _formKey.currentState?.validate();
+                  if (_extensionReason.id != null) {
+                    widget.onConfirm(
+                      ViolationExtensionPeriod(
+                        comments: _commentsController.value.text,
+                        extensionReason: _extensionReason,
+                        resolveDate: _newResolveDate,
+                      ),
+                    );
+                  }
+                },
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
